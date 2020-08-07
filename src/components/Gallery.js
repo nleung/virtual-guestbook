50b4f8f2704f55d1baf10279c1f8e40ca3e796d5
Nathan Leung <nleung@amazon.com>
2020-08-06 23:41:44 -0700

    Fix header image positioning and post margin

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,8 +8,6 @@ class Gallery extends React.Component {
       <div>
         <img class="Gallery-header-image"
           src="https://qa-media-api.xogrp.com/images/d0b995a2-0670-4093-ba5b-99611a8c427e~rt_auto-rs_3000.h?ordering=explicit"
-          width="1430px"
-          height="212px"
         />
         <div class="Gallery-header">MELISSA & SAMUEL'S GUESTBOOK</div>
         <div class="container px-0">
