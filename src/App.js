b77bf5e87df352a851697c90c4d75b9c029b58dc
Nathan Leung <nleung@amazon.com>
2021-07-06 16:41:00 -0700

    Change header flower, update font & colors

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Gallery from './components/Gallery'
 function App() {
   return (
     <div className="App">
-      <img className="Gallery-header-image" src="https://qa-media-api.xogrp.com/images/d0b995a2-0670-4093-ba5b-99611a8c427e~rt_auto-rs_3000.h?ordering=explicit" alt="Header" />
-      <div className="Gallery-header">TIFFANY & MICHAEL'S GUESTBOOK</div>
+      <div className="Gallery-header">
+        <div className="Gallery-header-text">TIFFANY & MICHAEL'S GUESTBOOK</div>
+      </div>
       <Gallery/>
     </div>
   );
